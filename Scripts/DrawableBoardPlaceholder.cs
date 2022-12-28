af86183d16fa8480802f509932ba77f40629bcf6
Emanuele Torelli <emanueletorelli@outlook.com>
2022-12-28 17:25:04 +0100

    PhotonViews runtime initializaiton WIP

--- Scripts/DrawableBoardPlaceholder.cs
+++ Scripts/DrawableBoardPlaceholder.cs
@@ -7,10 +7,12 @@ namespace Virtuademy.Placeholders
 {
     public class DrawableBoardPlaceholder : SceneComponentPlaceholderBase
     {
+        [SerializeField] private int instantiationId;
         [SerializeField] private Collider drawableArea;
         [SerializeField] private Transform drawingProjectionTransform;
         [SerializeField] private Role ownershipMask;
 
+        public int InstantiationId => instantiationId;
         public Collider DrawableArea => drawableArea;
         public Transform DrawingProjectionTransform => drawingProjectionTransform;
         public Role OwnershipMask => ownershipMask;
