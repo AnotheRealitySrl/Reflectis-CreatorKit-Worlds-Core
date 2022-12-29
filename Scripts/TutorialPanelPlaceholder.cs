5784136431cb5d42215f24235251af9b14667c92
Emanuele Torelli <emanueletorelli@outlook.com>
2022-12-29 11:03:39 +0100

    Add TutorialPanelPlaceholder

---
+++ Scripts/TutorialPanelPlaceholder.cs
@@ -0,0 +1,20 @@
+using SPACS.SDK.Transitions;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+namespace Virtuademy.Placeholders
+{
+    public class TutorialPanelPlaceholder : SceneComponentPlaceholderBase
+    {
+        [SerializeField] float loopInterval = 5f;
+        [SerializeField] List<CanvasGroup> images;
+        [SerializeField] List<AbstractTransitionProvider> instructions;
+
+        public float LoopInterval => loopInterval;
+        public List<CanvasGroup> Images => images;
+        public List<AbstractTransitionProvider> Instructions => instructions;
+    }
+}
+
