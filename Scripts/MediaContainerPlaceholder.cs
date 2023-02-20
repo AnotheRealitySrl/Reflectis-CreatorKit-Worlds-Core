965855d8fb67d6ebc5be395d16f6a4eba67c208e
Emanuele Torelli <emanueletorelli@outlook.com>
2023-02-20 12:00:56 +0100

    Add missing fields for video playback

--- Scripts/MediaContainerPlaceholder.cs
+++ Scripts/MediaContainerPlaceholder.cs
@@ -15,6 +15,7 @@ namespace Virtuademy.Placeholders
 
         [Header("Asset data")]
         [SerializeField] private string url;
+        [SerializeField] private bool videoPlayOnAwake;
 
         [Header("Media player controller")]
         [SerializeField] private Sprite mediaSprite;
@@ -23,10 +24,14 @@ namespace Virtuademy.Placeholders
 
         // Networking
         public int InitializationId => initializationId;
+
         // Component id
         public string AddressableKey => addressableKey;
+
         // Asset data
         public string Url => url;
+        public bool VideoPlayOnAwake => videoPlayOnAwake;
+
         // Media player controller
         public Sprite MediaSprite => mediaSprite;
         public List<Sprite> ControllerSprites => controllerSprites;
