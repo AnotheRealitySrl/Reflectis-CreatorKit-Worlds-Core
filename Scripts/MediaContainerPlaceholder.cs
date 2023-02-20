7e3c6ba641be7883fa587e46dfee67835ea17a05
Emanuele Torelli <emanueletorelli@outlook.com>
2023-02-20 11:22:24 +0100

    Add fields to MediaContainerPlaceholder

--- Scripts/MediaContainerPlaceholder.cs
+++ Scripts/MediaContainerPlaceholder.cs
@@ -1,19 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 namespace Virtuademy.Placeholders
 {
     public class MediaContainerPlaceholder : SceneComponentPlaceholderBase
     {
+        [Header("Networking")]
         [SerializeField] private int initializationId;
+
+        [Header("Component id")]
         [SerializeField] private string addressableKey;
+
+        [Header("Asset data")]
+        [SerializeField] private string url;
+
+        [Header("Media player controller")]
         [SerializeField] private Sprite mediaSprite;
         [SerializeField] private List<Sprite> controllerSprites;
         [SerializeField] private List<string> controllerCallbacks;
 
+        // Networking
         public int InitializationId => initializationId;
+        // Component id
         public string AddressableKey => addressableKey;
+        // Asset data
+        public string Url => url;
+        // Media player controller
         public Sprite MediaSprite => mediaSprite;
         public List<Sprite> ControllerSprites => controllerSprites;
         public List<string> ControllerCallbacks => controllerCallbacks;
