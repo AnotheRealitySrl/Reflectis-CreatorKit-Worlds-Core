a1266e78b7130becb42b5485f9cb381efae3fec3
Emanuele Torelli <emanueletorelli@outlook.com>
2023-02-22 19:33:46 +0100

    Add WebViewPlaceholder

---
+++ Scripts/WebViewPlaceholder.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Virtuademy.Placeholders
+{
+    public class WebViewPlaceholder : MonoBehaviour
+    {
+       
+    }
+}
+
