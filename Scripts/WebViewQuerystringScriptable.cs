07ca332b39b84336f90aa2e21cdb5cabc8eed90b
Emanuele Torelli <emanueletorelli@outlook.com>
2023-04-26 18:00:52 +0200

    Move WebViewQuerySstrinngScriptable to root repo

--- Scripts/WebViewQuerystringScriptable.cs
+++ Scripts/WebViewQuerystringScriptable.cs
@@ -17,8 +17,10 @@ public class WebViewQuerystringScriptable : SerializedScriptableObject
 {
     // These Dictionaries will be serialized by Odin.
     [SerializeField] private Dictionary<QuerystringParameter, string> querystringKeyMappings = new();
+    [SerializeField] private Dictionary<string, string> constantQuerystrings = new();
     [SerializeField] private Dictionary<Role, string> rolesMapping = new();
 
     public Dictionary<QuerystringParameter, string> QuerystringKeyMappings => querystringKeyMappings;
+    public Dictionary<string, string> ConstantQuerystrings => constantQuerystrings;
     public Dictionary<Role, string> RolesMapping => rolesMapping;
 }
