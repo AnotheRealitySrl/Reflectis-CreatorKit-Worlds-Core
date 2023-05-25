d08ccc35433ba629a7323e48fc9d9be557386c2c
Emanuele Torelli <emanueletorelli@outlook.com>
2023-05-25 11:03:39 +0200

    Add WebView3D placeholder properties

--- Scripts/WebViewPlaceholder3D.cs
+++ Scripts/WebViewPlaceholder3D.cs
@@ -16,19 +16,12 @@ namespace Virtuademy.Placeholders
         [SerializeField] private Transform targetTransform;
         [SerializeField] private RectTransform canvasTransform;
         [SerializeField] private RectTransform uiTransform;
+        [SerializeField] private float resolution;
+        [SerializeField] private bool keyboardEnabled;
 
         [Header("General settings")]
         [SerializeField] private string urlSandbox;
         [SerializeField] private string urlProduction;
-        //[SerializeField] private float resolution = 1.5f;
-        //[SerializeField] private bool native2DMode = true;
-        //[SerializeField] private float pixelDensity = 2f;
-
-        //[Header("WebView controller settings")]
-        //[SerializeField] private bool startWebViewVisible;
-
-        //[Header("Canvas Group settings")]
-        //[SerializeField] private bool ignoreParentGroups = true;
 
         [Header("Querystring formation")]
         [SerializeField] private WebViewQuerystringScriptable querystringMappings;
@@ -39,16 +32,11 @@ namespace Virtuademy.Placeholders
         public Transform TargetTransform => targetTransform;
         public RectTransform CanvasTransform => canvasTransform;
         public RectTransform UiTransform => uiTransform;
+        public float Resolution => resolution;
+        public bool KeyboardEnabled => keyboardEnabled;
 
         public string UrlSandbox => urlSandbox;
         public string UrlProduction => urlProduction;
-        //public float Resolution => resolution;
-        //public bool Native2DMode => native2DMode;
-        //public float PixelDensity => pixelDensity;
-
-        //public bool StartWebViewVisible => startWebViewVisible;
-
-        //public bool IgnoreParentGroups => ignoreParentGroups;
 
         public WebViewQuerystringScriptable QuerystringMappings => querystringMappings;
     }
