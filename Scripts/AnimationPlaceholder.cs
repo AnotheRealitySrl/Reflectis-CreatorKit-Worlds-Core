77a9f62bccf82e448776303ce3973189d8465724
DavideNanni <dnanni675@gmail.com>
2023-07-19 15:02:57 +0200

    Created Animation placeholder

---
+++ Scripts/AnimationPlaceholder.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Virtuademy.DTO;
+using static BaseAnimationController;
+
+namespace Virtuademy.Placeholders
+{
+    public class AnimationPlaceholder : SceneComponentPlaceholderBase
+    {
+        [Header("Network settings")]
+        [SerializeField] private Role ownershipMask;
+
+        [Header("Animation references")]
+        [SerializeField] private bool isLooping;
+        [SerializeField] private AnimationStates animationState;
+        [SerializeField] private Animator animator;
+        [SerializeField] private List<GameObject> connectables;
+        [SerializeField] private string animationToTrigger;
+
+
+        public Role OwnershipMask => ownershipMask;
+        public bool IsLooping => isLooping;
+        public AnimationStates AnimationState => animationState;
+        public Animator Animator  => animator;
+        public List<GameObject> Connectables  => connectables;
+        public string AnimationToTrigger  => animationToTrigger;
+    }
+}
