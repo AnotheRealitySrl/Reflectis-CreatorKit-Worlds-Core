9ec479d0c3952bad9db5ea243dc083570cd27eab
DavideNanni <dnanni675@gmail.com>
2023-07-19 10:03:27 +0200

    Updated speaker placeholder

--- Scripts/SpeakerPlaceholder.cs
+++ Scripts/SpeakerPlaceholder.cs
@@ -14,7 +14,7 @@ namespace Virtuademy.Placeholders
         [SerializeField] private float audioListenRange;
         [SerializeField] private bool isSpatialized;
         [SerializeField] private bool isLooping;
-        [SerializeField] private BaseSpeakerController.SpeakerState state;
+        [SerializeField] private BaseSpeakerController.SpeakerState state = BaseSpeakerController.SpeakerState.Stopped;
         [SerializeField] private List<GameObject> connectables; 
 
 
