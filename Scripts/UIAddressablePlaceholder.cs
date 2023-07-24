f4a0f5b63c2860a733eb1cb808aa554c666dcec4
Emanuele Torelli <emanueletorelli@outlook.com>
2023-07-24 16:30:24 +0200

    Remove toast system references

--- Scripts/UIAddressablePlaceholder.cs
+++ Scripts/UIAddressablePlaceholder.cs
@@ -1,5 +1,3 @@
-using Reflectis.SDK.UIKit.ToastSystem;
-
 using UnityEngine;
 
 namespace Virtuademy.Placeholders
@@ -9,11 +7,7 @@ namespace Virtuademy.Placeholders
         [Header("Addressable settings")]
         [SerializeField] private string addressableKey;
 
-        [Header("Toast system")]
-        [SerializeField] private InteractableByToast interactableByToast;
-
         public string AddressableKey => addressableKey;
-        public InteractableByToast InteractableByToast => interactableByToast;
     }
 }
 
