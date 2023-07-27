889265e92eab2124957ed2365ba28409cf491aab
Emanuele Torelli <emanueletorelli@outlook.com>
2023-07-27 17:19:58 +0200

    Fix merge issues

--- Scripts/AssetSpawnerPlaceholder.cs
+++ Scripts/AssetSpawnerPlaceholder.cs
@@ -1,9 +1,6 @@
-using GLTFast.Schema;
-using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
-using Virtuademy.DTO;
-using static BaseAssetSpawnerController;
 
 namespace Virtuademy.Placeholders
 {
@@ -14,29 +11,30 @@ namespace Virtuademy.Placeholders
             NotSpawned = 1,
             Spawned = 2
         }
+
         [Header("Network settings")]
         [SerializeField] private Role ownershipMask;
 
         [Header("Asset Spawner references")]
         [SerializeField] private List<GameObject> prefabsToInstantiate;
-        [SerializeField] private List<Transform>  posToSpawn;
+        [SerializeField] private List<Transform> posToSpawn;
         [SerializeField] private float spawnChance;
         [SerializeField] private bool haveToWait = false;
         //[SerializeField] private bool canMultipleSpawn = false;
         [SerializeField] private float timerToSpawn;
-        [SerializeField] private SpawnerState spawnerState = SpawnerState.NotSpawned;
+        [SerializeField] private ESpawnerState spawnerState = ESpawnerState.NotSpawned;
         [SerializeField] private List<GameObject> connectables;
         [SerializeField] private GameObject fatherConnecter;
 
-        public Role OwnershipMask  => ownershipMask;
-        public List<GameObject> PrefabsToInstantiate  => prefabsToInstantiate; 
-        public List<Transform> PosToSpawn => posToSpawn; 
-        public float SpawnChance => spawnChance; 
-        public bool HaveToWait  => haveToWait;
+        public Role OwnershipMask => ownershipMask;
+        public List<GameObject> PrefabsToInstantiate => prefabsToInstantiate;
+        public List<Transform> PosToSpawn => posToSpawn;
+        public float SpawnChance => spawnChance;
+        public bool HaveToWait => haveToWait;
         //public bool CanMultipleSpawn=> canMultipleSpawn;
         public float TimerToSpawn => timerToSpawn;
         public List<GameObject> Connectables => connectables;
-        public SpawnerState SpawnerState => spawnerState;
+        public ESpawnerState SpawnerState => spawnerState;
         public GameObject FatherConnecter => fatherConnecter;
     }
 }
