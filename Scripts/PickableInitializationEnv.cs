4736c5352449b4b289191be01ece728fceb0b366
DavideNanni <dnanni675@gmail.com>
2023-08-04 11:08:22 +0200

    Changed pickable initialization env script

--- Scripts/PickableInitializationEnv.cs
+++ Scripts/PickableInitializationEnv.cs
@@ -1,23 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Virtuademy.AuthoringTool;
-using Virtuademy.Core;
 
 public class PickableInitializationEnv : MonoBehaviour
 {
     [SerializeField] string addressableNameToInstantiate;
 
-
-    private void Awake()
-    {
-        InstantiateObject();
-    }
-
-    private async void InstantiateObject()
-    {
-        var sceneObj = SceneObjUtility.MakeSceneObj(addressableNameToInstantiate, addressableNameToInstantiate, ~Role.Guest);
-
-        var instantiatedObj = await sceneObj.Instantiate(new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
-    }
+    public string AddressableNameToInstantiate => addressableNameToInstantiate;
 }
