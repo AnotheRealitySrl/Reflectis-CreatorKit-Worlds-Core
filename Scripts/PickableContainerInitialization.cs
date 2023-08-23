6e03e1676fa45f228b4f597562ab8d7daee6b5ba
DavideNanni <dnanni675@gmail.com>
2023-08-23 16:30:05 +0200

    Created container to all pickable to get reference to initializate

---
+++ Scripts/PickableContainerInitialization.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickableContainerInitialization : MonoBehaviour
+{
+    public List<PickableInitializationEnv> EnvList { get; private set; }
+
+    private void Awake()
+    {
+        EnvList = new List<PickableInitializationEnv>();
+
+        var childPickables = GetComponentsInChildren<PickableInitializationEnv>();
+
+        foreach(var pickable in childPickables)
+        {
+            EnvList.Add(pickable);
+        }
+    }
+}
