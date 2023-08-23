2e22d5cc74ec41c181a096031580bc85c21951d1
DavideNanni <dnanni675@gmail.com>
2023-08-23 10:46:05 +0200

    Added reference of CMTemplateObj pickable

--- Scripts/PickableInitializationEnv.cs
+++ Scripts/PickableInitializationEnv.cs
@@ -2,13 +2,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using Virtuademy.Placeholders;
 
-[RequireComponent(typeof(BoxCollider)), RequireComponent(typeof(MeshRenderer)), RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(PickablePlaceholder)), RequireComponent(typeof(UsablePlaceholder))]
+[RequireComponent(typeof(Collider)), RequireComponent(typeof(MeshRenderer)), RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(PickablePlaceholder)), RequireComponent(typeof(UsablePlaceholder))]
 public class PickableInitializationEnv : MonoBehaviour
 {
     private Dictionary<UsablePlaceholder.UseType, string> usableMapper;
     private string addressableNameToInstantiate;
+    private InstantiatedCMTemplateObj instantiatedCMTemplateObj;
 
     public string AddressableNameToInstantiate => addressableNameToInstantiate;
+
+    public InstantiatedCMTemplateObj InstantiatedCMTemplateObj { get => instantiatedCMTemplateObj; set => instantiatedCMTemplateObj = value; }
 
     private void Awake()
     {
