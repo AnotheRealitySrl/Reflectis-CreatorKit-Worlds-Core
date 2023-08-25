12d39e152ebf17cdffa74b6ad29d618f49f0c7de
DavideNanni <dnanni675@gmail.com>
2023-08-25 11:54:07 +0200

    Added pickabledownloaded list

--- Scripts/PickableContainerInitialization.cs
+++ Scripts/PickableContainerInitialization.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PickableContainerInitialization : MonoBehaviour
 {
     public List<PickableInitializationEnv> EnvList { get; private set; }
+    public List<PickableDownloaded> DownloadedList { get; private set; }
 
     private void Awake()
     {
