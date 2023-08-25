7fbfcb71b71a58043e7e2d17c7f5615551e80259
DavideNanni <dnanni675@gmail.com>
2023-08-25 17:20:16 +0200

    Reapplied asset spawner multiple spawn logic

--- Scripts/AssetSpawnerPlaceholder.cs
+++ Scripts/AssetSpawnerPlaceholder.cs
@@ -18,7 +18,7 @@ namespace Virtuademy.Placeholders
         [SerializeField] private List<Transform> posToSpawn;
         [SerializeField] private float spawnChance;
         [SerializeField] private bool haveToWait = false;
-        //[SerializeField] private bool canMultipleSpawn = false;
+        [SerializeField] private bool canMultipleSpawn = false;
         [SerializeField] private float timerToSpawn;
         [SerializeField] private ESpawnerState spawnerState = ESpawnerState.NotSpawned;
         [SerializeField] private List<GameObject> connectables;
@@ -31,7 +31,7 @@ namespace Virtuademy.Placeholders
         public List<Transform> PosToSpawn => posToSpawn;
         public float SpawnChance => spawnChance;
         public bool HaveToWait => haveToWait;
-        //public bool CanMultipleSpawn=> canMultipleSpawn;
+        public bool CanMultipleSpawn=> canMultipleSpawn;
         public float TimerToSpawn => timerToSpawn;
         public List<GameObject> Connectables => connectables;
         public ESpawnerState SpawnerState => spawnerState;
