dd265c3a8e81af30011ac91dde1efa3a785455e0
DavideNanni <dnanni675@gmail.com>
2023-08-28 18:38:38 +0200

    Fixed issue on togglebehaviour

--- Scripts/ToggleBehaviour.cs
+++ Scripts/ToggleBehaviour.cs
@@ -3,24 +3,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class ToggleBehaviour : MonoBehaviour,IInteractable
+public class ToggleBehaviour : MonoBehaviour
 {
     [SerializeField] List<GameObject> behaviourComponents;
+    [SerializeField] Collider collider;
 
-    List<GameObject> behaviourComponentsReference = new List<GameObject>();
+    List<GameObject> behaviourComponentsReference;
 
     int currentBehaviourCycle;
+    bool isNotInteracted = false;
 
     public List<GameObject> BehaviourComponents => behaviourComponents;
-
     public GameObject InteractionTarget => throw new NotImplementedException();
-
+    public int CurrentBehaviourCycle => currentBehaviourCycle;
 
     private void Start()
     {
         behaviourComponentsReference = BehaviourComponents;
-
         foreach (var behaviour in behaviourComponentsReference)
         {
             try
@@ -32,26 +31,33 @@ public class ToggleBehaviour : MonoBehaviour,IInteractable
                 BehaviourComponents.Remove(behaviour);
             }
         }
-
         behaviourComponentsReference = BehaviourComponents;
     }
 
     public void Interact(Action completedCallback = null)
     {
-        behaviourComponents[currentBehaviourCycle].GetComponent<IInteractable>().Interact();
-
         try
         {
-            behaviourComponents[currentBehaviourCycle - 1].GetComponent<IInteractable>().Interact();
+            behaviourComponents[CurrentBehaviourCycle].GetComponent<IInteractable>().Interact();
+            if (currentBehaviourCycle != 0 && isNotInteracted)
+            {
+                behaviourComponents[CurrentBehaviourCycle - 1].GetComponent<IInteractable>().Interact();
+            }
+            isNotInteracted = true;
+            currentBehaviourCycle++;
         }
-        catch (ArgumentOutOfRangeException e)
+        catch
         {
-            behaviourComponents[behaviourComponents.Count].GetComponent<IInteractable>().Interact();
+            try
+            {
+                behaviourComponents[CurrentBehaviourCycle - 1].GetComponent<IInteractable>().Interact();
+                behaviourComponents[behaviourComponents.Count].GetComponent<IInteractable>().Interact();
+                currentBehaviourCycle = 1;
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                behaviourComponents[behaviourComponents.Count].GetComponent<IInteractable>().Interact();
+            }
         }
-    }
-
-    public void StopInteract(Action completedCallback = null)
-    {
-
     }
 }
