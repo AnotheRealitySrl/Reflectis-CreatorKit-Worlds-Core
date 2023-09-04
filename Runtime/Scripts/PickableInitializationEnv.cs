c8e629654471a4b3cf07eab95f33cd3cfd8d598a
DavideNanni <dnanni675@gmail.com>
2023-09-04 10:27:54 +0200

    Fixed commit error about duplicated scripts

--- Runtime/Scripts/PickableInitializationEnv.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-using Reflectis.SDK.CreatorKit;
-
-[RequireComponent(typeof(MeshRenderer)), RequireComponent(typeof(MeshFilter)), RequireComponent(typeof(PickablePlaceholder)), RequireComponent(typeof(UsablePlaceholder))]
-public class PickableInitializationEnv : MonoBehaviour
-{
-
-}
