9766f2f563bda822480cbf27a1bc2bc4777da695
DavideNanni <dnanni675@gmail.com>
2023-09-13 10:07:18 +0200

    Added dashboard placeholder spawn logic based by filter

--- Runtime/Scripts/SDK_BehaviourScripts/DashboardPlaceholder.cs
+++ Runtime/Scripts/SDK_BehaviourScripts/DashboardPlaceholder.cs
@@ -6,9 +6,19 @@ namespace Reflectis.SDK.CreatorKit
 {
     public class DashboardPlaceholder : SceneComponentPlaceholderNetwork
     {
+        public enum DashboardFilter
+        {
+            Environment,
+            Tag,
+            None
+        }
+
         [Header("DashboardData")]
         [SerializeField] private int dashboardID;
+        [SerializeField] private DashboardFilter filter;
 
         public int DashboardID => dashboardID;
+
+        public DashboardFilter Filter => filter;
     }
 }
