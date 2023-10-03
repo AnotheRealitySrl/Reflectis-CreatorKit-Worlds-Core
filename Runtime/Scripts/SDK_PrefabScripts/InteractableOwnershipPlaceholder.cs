ff742c771d9615c8f24437d20fcabe0b31db2932
Emanuele Torelli <emanueletorelli@outlook.com>
2023-10-03 18:28:25 +0200

    Fix InteractableOwnershipPlaceholder namespace

--- Runtime/Scripts/SDK_PrefabScripts/InteractableOwnershipPlaceholder.cs
+++ Runtime/Scripts/SDK_PrefabScripts/InteractableOwnershipPlaceholder.cs
@@ -1,17 +1,16 @@
-using System.Collections.Generic;
-
 using UnityEngine;
-using UnityEngine.Events;
 
-namespace Virtuademy.Placeholders
+namespace Reflectis.SDK.CreatorKit
 {
     public class InteractableOwnershipPlaceholder : SceneComponentPlaceholderNetwork
     {
         public Transform grabbableItem; //The item to be grabbed, containing photonview
         public Role ownershipMask; //The possible users that can grab the item
 
-        private void Awake(){
-            if(grabbableItem == null){
+        private void Awake()
+        {
+            if (grabbableItem == null)
+            {
                 grabbableItem = gameObject.transform;
             }
         }
