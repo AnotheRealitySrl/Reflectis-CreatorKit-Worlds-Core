5db69e14bfbc3aef7e8a32cc9f1380cf6243ac7c
Emanuele Torelli <emanueletorelli@outlook.com>
2023-10-06 15:57:07 +0200

    Fix scene change issue

--- Runtime/Scripts/ActionsScriptables/CharacterActivationScriptableAction.cs
+++ Runtime/Scripts/ActionsScriptables/CharacterActivationScriptableAction.cs
@@ -1,10 +1,10 @@
+using Reflectis.SDK.Avatars;
+using Reflectis.SDK.Core;
+using Reflectis.SDK.Interaction;
+
 using System;
 
 using UnityEngine;
-
-using Reflectis.SDK.Core;
-using Reflectis.SDK.Avatars;
-using Reflectis.SDK.Interaction;
 
 [CreateAssetMenu(menuName = "AnotheReality/Utilities/ActivateCharacterScriptableAction", fileName = "ActivateCharacterScriptableAction")]
 public class CharacterActivationScriptableAction : ActionScriptable
@@ -14,6 +14,6 @@ public class CharacterActivationScriptableAction : ActionScriptable
     public override void Action(Action completedCallback)
     {
         SM.GetSystem<AvatarSystem>().EnableAvatarInstanceMeshes(activate);
-        completedCallback();
+        completedCallback?.Invoke();
     }
 }
