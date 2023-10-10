1c2c91c31824920be933058fc2e75817c8f7b487
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2023-10-10 17:36:56 +0200

    Added fields to mascottePlaceholder

--- Runtime/Scripts/SDK_PrefabScripts/MascottePlaceholder.cs
+++ Runtime/Scripts/SDK_PrefabScripts/MascottePlaceholder.cs
@@ -1,21 +1,24 @@
+using Reflectis.SDK.Interaction;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Reflectis.SDK.CreatorKit
 {
-    public class MascottePlaceholder : MonoBehaviour
+    public class MascottePlaceholder : SceneComponentPlaceholderBase
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [SerializeField]
+        private Animator animator;
+        [SerializeField]
+        private Transform nameInputParent;
+        [SerializeField]
+        private Transform uiParent;
+        [SerializeField]
+        private BaseInteractableGO pan;
 
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        public Animator Animator { get => animator; set => animator = value; }
+        public Transform NameInputParent { get => nameInputParent; set => nameInputParent = value; }
+        public Transform UiParent { get => uiParent; set => uiParent = value; }
+        public BaseInteractableGO Pan { get => pan; set => pan = value; }
     }
 }
