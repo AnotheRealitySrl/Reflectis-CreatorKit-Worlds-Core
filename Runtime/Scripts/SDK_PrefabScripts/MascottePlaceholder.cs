f75e35eb7a1c9c5c08a217842dec7c28401a29d1
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2023-10-12 16:49:34 +0200

    Updated mascotte placeholder variables

--- Runtime/Scripts/SDK_PrefabScripts/MascottePlaceholder.cs
+++ Runtime/Scripts/SDK_PrefabScripts/MascottePlaceholder.cs
@@ -1,4 +1,5 @@
 using Reflectis.SDK.Interaction;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,18 +8,37 @@ namespace Reflectis.SDK.CreatorKit
 {
     public class MascottePlaceholder : SceneComponentPlaceholderBase
     {
+        [Serializable]
+        public struct UISize
+        {
+            public float width;
+            public float height;
+        }
+
         [SerializeField]
         private Animator animator;
         [SerializeField]
-        private Transform nameInputParent;
-        [SerializeField]
         private Transform uiParent;
         [SerializeField]
+        private UISize uiSize;
+        [SerializeField]
         private BaseInteractableGO pan;
+        [SerializeField]
+        private string[] faqCategory;
 
-        public Animator Animator { get => animator; set => animator = value; }
-        public Transform NameInputParent { get => nameInputParent; set => nameInputParent = value; }
-        public Transform UiParent { get => uiParent; set => uiParent = value; }
-        public BaseInteractableGO Pan { get => pan; set => pan = value; }
+        public Animator Animator { get => animator; }
+        public Transform UiParent { get => uiParent; }
+        public BaseInteractableGO Pan { get => pan; }
+        public UISize UiSize { get => uiSize;  }
+        public string[] FaqCategory { get => faqCategory;  }
+
+        private void OnDrawGizmos()
+        {
+            if (uiParent != null)
+            {
+                Gizmos.color = Color.white;
+                Gizmos.DrawWireCube(uiParent.position, new Vector3(UiSize.width, UiSize.height, 0.01f));
+            }
+        }
     }
 }
