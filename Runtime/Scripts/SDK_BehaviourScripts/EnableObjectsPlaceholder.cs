35c0bc8ee45778819a0ca1343cd0cd9d88efbc3c
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2023-11-02 15:42:04 +0100

    Added enable objects placeholder

---
+++ Runtime/Scripts/SDK_BehaviourScripts/EnableObjectsPlaceholder.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Reflectis.SDK.CreatorKit
+{
+    public class EnableObjectsPlaceholder : SceneComponentPlaceholderBase
+    {
+        [SerializeField]
+        private List<GameObject> disableInVR = new List<GameObject>();
+
+        [SerializeField]
+        private List<GameObject> disableInWebGL = new List<GameObject>();
+
+        public List<GameObject> DisableInVR { get => disableInVR; set => disableInVR = value; }
+        public List<GameObject> DisableInWebGL { get => disableInWebGL; set => disableInWebGL = value; }
+    }
+}
