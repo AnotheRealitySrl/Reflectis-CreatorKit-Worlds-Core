3d9321ee658e699d3483c5b000a03732f3d9a57d
Lucas <>
2023-11-16 14:15:31 +0100

    Add taskcompletedPing placeholder

---
+++ Runtime/Scripts/Utilities/TaskCompletedPingPlaceholder.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Reflectis.SDK.CreatorKit
+{
+    public enum TasksToPing { MacroTasksOnly, FirstSubtasksLine, AllTasks }
+
+    public class TaskCompletedPingPlaceholder : SceneComponentPlaceholderBase
+    {
+        [SerializeField]
+        private TasksToPing taskToPingSetting;
+
+        public TasksToPing TaskToPingSetting { get => taskToPingSetting;}
+    }
+}
