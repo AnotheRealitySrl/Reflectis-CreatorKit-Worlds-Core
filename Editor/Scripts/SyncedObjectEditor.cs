aacf35a126ba1bf73d253363d9d187ad8bbfe836
DavideNanni <dnanni675@gmail.com>
2023-11-20 17:27:29 +0100

    Cleared and polished synced object editor and synced variables editor

--- Editor/Scripts/SyncedObjectEditor.cs
+++ Editor/Scripts/SyncedObjectEditor.cs
@@ -7,25 +7,8 @@ namespace Reflectis.SDK.CreatorKit
     [CustomEditor(typeof(SyncedObject))]
     public class SyncedObjectEditor : UnityEditor.Editor
     {
-        private bool _initialized;
         //used to hide the script field
         private static readonly string[] _excludedProperties = new string[] { "m_Script" };
-        private Texture2D _backgroundTexture;
-        private Texture2D _subBackgroundTexture;
-        private Texture2D _buttonTexture;
-        private Texture2D _iconTexture;
-        private Texture2D _caretUpTexture;
-        private Texture2D _caretDownTexture;
-
-        protected GUIStyle _warningStyle;
-        private GUIStyle _areaStyle;
-        private GUIStyle _subAreaStyle;
-        private GUIStyle _logoStyle;
-        private GUIStyle _titleStyle;
-        private GUIStyle _subTitleStyle;
-        private GUIStyle _helpButtonStyle;
-        private GUIStyle _caretStyle;
-        private GUIStyle _hiddenToggleButtonStyle;
 
         private UnityEditor.Editor _variablesEditor;
         private SerializedProperty _syncTransformProp;
@@ -39,215 +22,11 @@ namespace Reflectis.SDK.CreatorKit
             }
 
             _syncTransformProp = serializedObject.FindProperty(nameof(SyncedObject.syncTransform));
-            //_syncRigidbodyProp = serializedObject.FindProperty(nameof(SyncedObject.syncRigidbody));
-            //_destroyOnDisconnectProp = serializedObject.FindProperty(nameof(SyncedObject.destroyOnCreatorDisconnect));
         }
-
-        //private void InitializeIfNecessary(UnityEngine.Object target)
-        //{
-        //    if (_initialized)
-        //    {
-        //        return;
-        //    }
-
-        //    if (target is SpatialComponentBase component)
-        //    {
-        //        _prettyName = component.prettyName;
-        //        _tooltip = component.tooltip;
-        //        _documentationURL = component.documentationURL;
-        //        _isExperimental = component.isExperimental;
-        //    }
-        //    else if (target is SpatialScriptableObjectBase scriptableObject)
-        //    {
-        //        _prettyName = scriptableObject.prettyName;
-        //        _tooltip = scriptableObject.tooltip;
-        //        _documentationURL = scriptableObject.documentationURL;
-        //        _isExperimental = scriptableObject.isExperimental;
-        //    }
-        //    else
-        //    {
-        //        _prettyName = target.GetType().Name;
-        //        _tooltip = "";
-        //        _documentationURL = "";
-        //        _isExperimental = false;
-        //    }
-
-        //    GUIContent c = EditorGUIUtility.ObjectContent(target, target.GetType());
-        //    if (c.image == null)
-        //    {
-        //        _iconTexture = SpatialGUIUtility.LoadGUITexture("GUI/SpatialLogo.png");
-        //    }
-        //    else
-        //    {
-        //        _iconTexture = (Texture2D)c.image;
-        //    }
-
-        //    _backgroundTexture = SpatialGUIUtility.LoadGUITexture("GUI/TooltipBackground.png");
-        //    _subBackgroundTexture = SpatialGUIUtility.LoadGUITexture("GUI/TooltipSubBackground.png");
-        //    _buttonTexture = SpatialGUIUtility.LoadGUITexture("GUI/ButtonBackground.png");
-        //    _caretUpTexture = SpatialGUIUtility.LoadGUITexture("GUI/CaretUp.png");
-        //    _caretDownTexture = SpatialGUIUtility.LoadGUITexture("GUI/CaretDown.png");
-
-        //    _warningStyle = new GUIStyle()
-        //    {
-        //        padding = new RectOffset(0, 0, 5, 0),
-        //        fontStyle = FontStyle.Bold,
-        //        wordWrap = true,
-        //        alignment = TextAnchor.MiddleLeft,
-        //    };
-        //    _warningStyle.normal.textColor = new Color(1, .66f, 0f);
-        //    _warningStyle.active.textColor = new Color(1, .66f, 0f);
-
-        //    _areaStyle = new GUIStyle()
-        //    {
-        //        border = new RectOffset(8, 8, 8, 8),
-        //        padding = new RectOffset(8, 8, 8, 8),
-        //        alignment = TextAnchor.MiddleLeft,
-        //    };
-        //    _areaStyle.normal.background = _backgroundTexture;
-        //    _areaStyle.normal.textColor = Color.white;
-
-        //    _subAreaStyle = new GUIStyle()
-        //    {
-        //        border = new RectOffset(8, 8, 8, 8),
-        //        padding = new RectOffset(16, 8, 8, 8),
-        //        alignment = TextAnchor.MiddleLeft,
-        //    };
-        //    _subAreaStyle.normal.background = _subBackgroundTexture;
-        //    _subAreaStyle.normal.textColor = Color.white;
-
-        //    _logoStyle = new GUIStyle()
-        //    {
-        //        fixedHeight = 28,
-        //        fixedWidth = 28,
-        //        padding = new RectOffset(0, 0, 0, 0),
-        //        border = new RectOffset(0, 0, 0, 0),
-        //    };
-
-        //    _caretStyle = new GUIStyle()
-        //    {
-        //        contentOffset = new Vector2(0, 4),
-        //    };
-
-        //    _titleStyle = new GUIStyle()
-        //    {
-        //        fontStyle = FontStyle.Bold,
-        //        fontSize = 20,
-        //        wordWrap = true,
-        //        alignment = TextAnchor.MiddleLeft,
-        //    };
-        //    _titleStyle.normal.textColor = Color.white;
-
-        //    _subTitleStyle = new GUIStyle()
-        //    {
-        //        fontSize = 12,
-        //        wordWrap = true,
-        //        richText = true,
-        //    };
-        //    _subTitleStyle.normal.textColor = new Color(1, 1, 1, .75f);
-
-        //    _helpButtonStyle = new GUIStyle()
-        //    {
-        //        border = new RectOffset(8, 8, 8, 8),
-        //        padding = new RectOffset(4, 4, 4, 4),
-        //        alignment = TextAnchor.MiddleCenter,
-        //        fontStyle = FontStyle.Bold,
-        //        fontSize = 14,
-        //        fixedWidth = 48,
-        //    };
-        //    _helpButtonStyle.active.background = _buttonTexture;
-        //    _helpButtonStyle.normal.background = _buttonTexture;
-
-        //    // an invisible button style
-        //    _hiddenToggleButtonStyle = new GUIStyle()
-        //    {
-        //    };
-        //}
 
         public override void OnInspectorGUI()
         {
-            var editorTarget = target as UnityEngine.Object;
-            //InitializeIfNecessary(editorTarget);
             serializedObject.Update();
-
-            bool showSubMenu = EditorPrefs.GetBool("_InspExpand_" + target.GetType().Name, true);
-
-            //GUILayout.Space(-4);// Top margin hack
-            //GUILayout.BeginHorizontal();
-            //GUILayout.Space(-18);// Left margin hack
-
-            //! START
-            //GUILayout.BeginVertical();
-            //GUILayout.BeginVertical(_areaStyle);
-            //{
-            //    GUILayout.BeginHorizontal();
-            //    {
-            //        GUILayout.Box(_iconTexture, _logoStyle);
-            //        GUILayout.Space(4);
-
-            //        // Align to the center of the icon
-            //        GUILayout.BeginVertical();
-            //        GUILayout.FlexibleSpace();
-            //        //GUILayout.Label(_prettyName, _titleStyle);
-            //        GUILayout.FlexibleSpace();
-            //        GUILayout.EndVertical();
-
-            //        GUILayout.FlexibleSpace();
-
-            //        GUILayout.Box(showSubMenu ? _caretUpTexture : _caretDownTexture, _caretStyle);
-            //        GUILayout.BeginVertical();
-            //        GUILayout.FlexibleSpace();
-            //        GUILayout.Label(showSubMenu ? "Less" : "More");
-            //        GUILayout.FlexibleSpace();
-            //        GUILayout.EndVertical();
-            //    }
-            //    GUILayout.EndHorizontal();
-
-            //}
-            //GUILayout.EndVertical();
-
-            //GUILayout.Space(-44);
-            //if (GUILayout.Button("", GUIStyle.none, GUILayout.Height(44)))
-            //{
-            //    EditorPrefs.SetBool("_InspExpand_" + target.GetType().Name, !showSubMenu);
-            //}
-
-            if (showSubMenu)
-            {
-                //GUILayout.Space(-5);// account for texture corners
-                //                    // * Subtitle
-                //GUILayout.BeginVertical(_subAreaStyle);
-                //if (_isExperimental)
-                //{
-                //    GUILayout.Label("Experimental Feature", _warningStyle);
-                //}
-                //else
-                //{
-                //    GUILayout.Space(4);
-                //}
-                //GUILayout.Label(_tooltip, _subTitleStyle);
-                //if (!string.IsNullOrEmpty(_documentationURL))
-                //{
-                //    GUILayout.Space(2);
-                //    GUILayout.BeginHorizontal();// Create a new area to delete the extra area LinkButton creates...
-                //    GUILayout.Space(-2);
-                //    if (EditorGUILayout.LinkButton("Documentation"))
-                //    {
-                //        GUILayout.Space(2);
-                //        Application.OpenURL(_documentationURL);
-                //    }
-                //    GUILayout.EndHorizontal();
-                //    GUILayout.Space(6);
-                //}
-                //GUILayout.EndVertical();
-            }
-            //! END
-            //GUILayout.EndVertical();
-
-            //GUILayout.Space(-4);// Right Margin hack
-            //GUILayout.EndHorizontal();
-
-            //GUILayout.Space(6);// Margin between tooltip and inspector
 
             DrawFields();
 
@@ -294,37 +73,8 @@ namespace Reflectis.SDK.CreatorKit
             InitializePropertiesIfNecessary();
             SyncedObject syncedObject = target as SyncedObject;
 
-            if (syncedObject.gameObject.GetComponentsInParent<SyncedObject>().Length > 1)
-            {
-                //SpatialGUIUtility.HelpBox("A Synced Object can not be the child of another Synced Object", SpatialGUIUtility.HelpSectionType.Error);
-            }
-
             EditorGUILayout.PropertyField(_syncTransformProp);
 
-            if (syncedObject.GetComponent<Rigidbody>() == null)
-            {
-                GUI.enabled = false;
-                //EditorGUILayout.PropertyField(_syncRigidbodyProp);
-                //syncedObject.syncRigidbody = false;
-                GUI.enabled = true;
-            }
-            else
-            {
-                //EditorGUILayout.PropertyField(_syncRigidbodyProp);
-            }
-
-            //bool enableDestroyOnDisconnect = (syncedObject.gameObject.scene.name == null || !syncedObject.gameObject.scene.name.Equals(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name));
-            // if we enabled save with space, then don't also allow destroy on disconnect
-            //enableDestroyOnDisconnect &= !syncedObject.saveWithSpace;
-
-            //if (!enableDestroyOnDisconnect)
-            //{
-            //    GUI.enabled = enableDestroyOnDisconnect;
-            //    syncedObject.destroyOnCreatorDisconnect = false;
-            //}
-
-            //we are a prefab. Show the destroy option
-            //EditorGUILayout.PropertyField(_destroyOnDisconnectProp);
             GUI.enabled = true;
 
             GUILayout.Space(8);
@@ -332,10 +82,16 @@ namespace Reflectis.SDK.CreatorKit
             //Embed the synced variables inspector
             if (syncedObject.TryGetComponent(out SyncedVariables syncedVariables))
             {
+                GUIStyle boldStyle = new GUIStyle(GUI.skin.label);
+                boldStyle.fontStyle = FontStyle.Bold;
+
+                GUILayout.Label("List of Variables", boldStyle);
+
                 if (_variablesEditor == null || _variablesEditor.target != syncedVariables)
                 {
                     _variablesEditor = UnityEditor.Editor.CreateEditor(syncedVariables);
                 }
+
                 _variablesEditor.OnInspectorGUI();
             }
             else
--- Editor/Scripts/SyncedVariablesEditor.cs
+++ Editor/Scripts/SyncedVariablesEditor.cs
@@ -11,19 +11,6 @@ namespace Reflectis.SDK.CreatorKit
     [CustomEditor(typeof(SyncedVariables))]
     public class SyncedVariablesEditor : UnityEditor.Editor
     {
-        private bool _initialized = false;
-        private Texture2D _backgroundTexture;
-        private Texture2D _buttonTexture;
-        private Texture2D _docsLinkTexture;
-        private GUIStyle _areaStyle;
-        private GUIStyle _titleStyle;
-        private GUIStyle _subTitleStyle;
-        private GUIStyle _helpButtonStyle;
-        private GUIStyle _syncedStyle;
-        private GUIStyle _unsyncedStyle;
-
-        private static readonly string[] _excludedProperties = new string[] { "m_Script" };
-
         void OnEnable()
         {
             var variables = target as SyncedVariables;
@@ -33,94 +20,10 @@ namespace Reflectis.SDK.CreatorKit
             }
         }
 
-        private void InitializeIfNecessary()
-        {
-            if (_initialized)
-            {
-                return;
-            }
-
-            _initialized = true;
-
-            //_backgroundTexture = SpatialGUIUtility.LoadGUITexture("GUI/TooltipBackground.png");
-            //_buttonTexture = SpatialGUIUtility.LoadGUITexture("GUI/ButtonBackground.png");
-            //_docsLinkTexture = SpatialGUIUtility.LoadGUITexture("Icons/icon_docsLink.png");
-
-            //_areaStyle = new GUIStyle()
-            //{
-            //    border = new RectOffset(8, 8, 8, 8),
-            //    padding = new RectOffset(8, 8, 8, 8),
-            //};
-            //_areaStyle.normal.background = _backgroundTexture;
-            //_areaStyle.normal.textColor = Color.white;
-
-            //_titleStyle = new GUIStyle()
-            //{
-            //    fontStyle = FontStyle.Bold,
-            //    fontSize = 16,
-            //    wordWrap = true,
-            //};
-            //_titleStyle.normal.textColor = Color.white;
-
-            //_subTitleStyle = new GUIStyle()
-            //{
-            //    fontSize = 11,
-            //    wordWrap = true,
-            //    richText = true,
-            //};
-            //_subTitleStyle.normal.textColor = new Color(1, 1, 1, .75f);
-
-            //_helpButtonStyle = new GUIStyle()
-            //{
-            //    border = new RectOffset(8, 8, 8, 8),
-            //    padding = new RectOffset(4, 4, 4, 4),
-            //    alignment = TextAnchor.MiddleCenter,
-            //    fontStyle = FontStyle.Bold,
-            //    fontSize = 14,
-            //    fixedWidth = 48,
-            //};
-            //_helpButtonStyle.active.background = _buttonTexture;
-            //_helpButtonStyle.normal.background = _buttonTexture;
-
-            //_syncedStyle = new GUIStyle(GUI.skin.button)
-            //{
-            //    fontStyle = FontStyle.Bold,
-            //};
-            //_syncedStyle.normal.textColor = new Color32(157, 204, 57, 255);
-
-            //_unsyncedStyle = new GUIStyle(GUI.skin.button)
-            //{
-            //    fontStyle = FontStyle.Bold,
-            //};
-            //_unsyncedStyle.normal.textColor = EditorGUIUtility.isProSkin ? new Color32(200, 200, 200, 255) : new Color32(0, 0, 0, 255);
-        }
-
         public override void OnInspectorGUI()
         {
-            InitializeIfNecessary();
             var syncedVariables = target as SyncedVariables;
             serializedObject.Update();
-
-
-            //GUILayout.BeginVertical(_areaStyle);
-            //{
-            //    GUILayout.BeginHorizontal();
-            //    {
-            //        GUILayout.Label(syncedVariables.prettyName, _titleStyle);
-            //        if (!string.IsNullOrEmpty(syncedVariables.documentationURL))
-            //        {
-            //            GUILayout.Space(4);
-            //            if (GUILayout.Button(_docsLinkTexture, _helpButtonStyle))
-            //            {
-            //                Application.OpenURL(syncedVariables.documentationURL);
-            //            }
-            //        }
-            //    }
-            //    GUILayout.EndHorizontal();
-            //    GUILayout.Space(6);
-            //    GUILayout.Label(syncedVariables.tooltip, _subTitleStyle);
-            //}
-            //GUILayout.EndVertical();
 
             GUILayout.Space(8);
 
@@ -202,10 +105,8 @@ namespace Reflectis.SDK.CreatorKit
 
                 if (!hasSomeVariables)
                 {
-                    //SpatialGUIUtility.HelpBox(
-                    //    "No sync-able variables found.",
-                    //    "Define a new variable in the Visual Scripting Variables component with a compatible type.",
-                    //    SpatialGUIUtility.HelpSectionType.Info);
+                    GUILayout.Space(5);
+                    EditorGUILayout.LabelField("(No valid variables defined.)", syncedVariables.variableSettings.Count.ToString());
                 }
 
                 // search through VariableSettings for any that are no longer valid
@@ -226,10 +127,7 @@ namespace Reflectis.SDK.CreatorKit
                 if (toRemove.Count > 0)
                 {
                     GUILayout.Space(8);
-                    //SpatialGUIUtility.HelpBox(
-                    //    "Previously declared synced variables are no longer valid.",
-                    //    "You probably renamed or changed the type of a variable. No worries! Feel free to clear these if this was not a mistake.",
-                    //    SpatialGUIUtility.HelpSectionType.Warning);
+
                     GUILayout.Space(4);
                     foreach (SyncedVariables.Data variableSetting in toRemove)
                     {
@@ -250,10 +148,6 @@ namespace Reflectis.SDK.CreatorKit
                     }
                 }
             }
-            //else
-            //{
-            //    SpatialGUIUtility.HelpBox("Visual Scripting Variables component is missing", SpatialGUIUtility.HelpSectionType.Error);
-            //}
         }
 
         private bool TypeIsSyncable(Type type)
