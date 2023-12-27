1e5fcf8c3994425e5222350b4bf7179f98c7cc79
Emanuele Torelli <emanueletorelli@outlook.com>
2023-12-27 14:56:46 +0100

    AssetReferences is now IEquatable

--- Runtime/Scripts/SDK_UsefulScripts/AssetReferences.cs
+++ Runtime/Scripts/SDK_UsefulScripts/AssetReferences.cs
@@ -1,14 +1,19 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+
 using UnityEngine;
 
 namespace Reflectis.SDK.CreatorKit
 {
-    [System.Serializable]
-    public class AssetReferences 
+    [Serializable]
+    public class AssetReferences : IEquatable<AssetReferences>
     {
         public GameObject prefabToInstantiate;
         public Transform posToSpawn;
         public float spawnChance;
+
+        public bool Equals(AssetReferences other) =>
+                prefabToInstantiate == other.prefabToInstantiate &&
+                posToSpawn == other.posToSpawn &&
+                spawnChance == other.spawnChance;
     }
 }
