968f99cf06c852c104d5ca15464ccadcc8681fa3
Emanuele Torelli <emanueletorelli@outlook.com>
2024-01-30 17:00:20 +0100

    Remove Role field in InteractableOwnerhsipPlaceholder

--- Runtime/Scripts/SDK_PrefabScripts/InteractableOwnershipPlaceholder.cs
+++ Runtime/Scripts/SDK_PrefabScripts/InteractableOwnershipPlaceholder.cs
@@ -5,7 +5,6 @@ namespace Reflectis.SDK.CreatorKit
     public class InteractableOwnershipPlaceholder : SceneComponentPlaceholderNetwork
     {
         public Transform grabbableItem; //The item to be grabbed, containing photonview
-        public Role ownershipMask; //The possible users that can grab the item
 
         private void Awake()
         {
