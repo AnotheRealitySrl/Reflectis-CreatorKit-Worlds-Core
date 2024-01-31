be878682914e7acaea3ce475d04ac344ca0afe9e
Emanuele Torelli <emanueletorelli@outlook.com>
2024-01-31 11:51:43 +0100

    Update BigScreen and BigScreenPlaceholder

--- Runtime/Scripts/SDK_PrefabScripts/BigScreenPlaceholder.cs
+++ Runtime/Scripts/SDK_PrefabScripts/BigScreenPlaceholder.cs
@@ -1,21 +1,34 @@
 using Reflectis.SDK.ClientModels;
 using Reflectis.SDK.CreatorKit;
+using Reflectis.SDK.Utilities;
 
 using UnityEngine;
 
 public class BigScreenPlaceholder : SceneComponentPlaceholderNetwork
 {
+    [Header("Screen")]
     [SerializeField, Tooltip("The transform that contains the body of the media player. Do not change this reference unless a custom prefab is needed.")]
     private Transform contentTransform;
     [SerializeField, Tooltip("The transform that contains the media being reproduced. Do not change this reference unless a custom prefab is needed.")]
     private Transform screenTransform;
+    [SerializeField/*, Range(0.5f, 10)*/, Tooltip("The width of the screen.")]
+    [OnChangedCall(nameof(OnWidthChanged))]
+    private float screenWidth = 1.5f;
+    [SerializeField/*, Range(0.5f, 10)*/, Tooltip("The height of the screen.")]
+    [OnChangedCall(nameof(OnHeightChanged))]
+    private float screenHeight = 1f;
+
+    [Header("Default media settings")]
     [SerializeField, Tooltip("If this flag is set, a default media is loaded in the big screen.")]
     private bool defaultMedia;
     [SerializeField, Tooltip("The type of media being loaded by default. Valid only if defaultMedia is set to true")]
+    [DrawIf(nameof(defaultMedia), true)]
     private FileTypeExt mediaType;
     [SerializeField, Tooltip("The url of the media being loaded by default. Valid only if defaultMedia is set to true")]
+    [DrawIf(nameof(defaultMedia), true)]
     private string defaultUrl;
     [SerializeField, Tooltip("If set to true, it won't be possible to change the media on this screen")]
+    [DrawIf(nameof(defaultMedia), true)]
     private bool isLocked;
 
     public Transform ScreenTransform => screenTransform;
@@ -24,4 +37,16 @@ public class BigScreenPlaceholder : SceneComponentPlaceholderNetwork
     public FileTypeExt MediaType => mediaType;
     public string DefaultUrl => defaultUrl;
     public bool IsLocked => isLocked;
+
+    public void OnWidthChanged()
+    {
+        Debug.Log(screenWidth);
+        screenTransform.localScale = new Vector3(screenWidth, screenTransform.localScale.y, screenTransform.localScale.z);
+    }
+
+    public void OnHeightChanged()
+    {
+        Debug.Log(screenWidth);
+        screenTransform.localScale = new Vector3(screenTransform.localScale.x, screenHeight, screenTransform.localScale.z);
+    }
 }
