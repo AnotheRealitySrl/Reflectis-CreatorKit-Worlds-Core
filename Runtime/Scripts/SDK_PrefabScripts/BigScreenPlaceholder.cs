e5d7a95deea102d12c477766c04c6f1cd8378cc2
Emanuele Torelli <emanueletorelli@outlook.com>
2024-02-01 16:57:38 +0100

    Update BigScreen prefab and placeholders

--- Runtime/Scripts/SDK_PrefabScripts/BigScreenPlaceholder.cs
+++ Runtime/Scripts/SDK_PrefabScripts/BigScreenPlaceholder.cs
@@ -6,19 +6,26 @@ using UnityEngine;
 
 public class BigScreenPlaceholder : SceneComponentPlaceholderNetwork
 {
-    [Header("Screen")]
+    [Header("Screen references. \nDo not change unless you are making a custom prefab.")]
     [SerializeField, Tooltip("The transform that contains the body of the media player. Do not change this reference unless a custom prefab is needed.")]
     private Transform contentTransform;
     [SerializeField, Tooltip("The transform that contains the media being reproduced. Do not change this reference unless a custom prefab is needed.")]
     private Transform screenTransform;
+    [SerializeField, Tooltip("The transform that is used by the camera in case of a pan towards the screen.")]
+    private Transform cameraPanTransform;
+
+    [Header("Screen size utilities")]
     [SerializeField/*, Range(0.5f, 10)*/, Tooltip("The width of the screen.")]
     [OnChangedCall(nameof(OnWidthChanged))]
     private float screenWidth = 1.5f;
     [SerializeField/*, Range(0.5f, 10)*/, Tooltip("The height of the screen.")]
     [OnChangedCall(nameof(OnHeightChanged))]
     private float screenHeight = 1f;
+    [SerializeField/*, Range(0.5f, 10)*/, Tooltip("The distance of the pan transform.")]
+    //[OnChangedCall(nameof(OnHeightChanged))]
+    private float cameraPanDistance = 1f;
 
-    [Header("Default media settings")]
+    [Header("Should a default media be preloaded on the screen?")]
     [SerializeField, Tooltip("If this flag is set, a default media is loaded in the big screen.")]
     private bool defaultMedia;
     [SerializeField, Tooltip("The type of media being loaded by default. Valid only if defaultMedia is set to true")]
