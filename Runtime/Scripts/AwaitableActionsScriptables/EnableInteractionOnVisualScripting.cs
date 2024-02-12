b4b030ee735302ab26200f8ad5f86e517951e4fa
DavideNanni <dnanni675@gmail.com>
2024-02-12 16:30:07 +0100

    added new check for list event names

--- Runtime/Scripts/AwaitableActionsScriptables/EnableInteractionOnVisualScripting.cs
+++ Runtime/Scripts/AwaitableActionsScriptables/EnableInteractionOnVisualScripting.cs
@@ -1,4 +1,5 @@
 using Reflectis.SDK.InteractionNew;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,7 +9,7 @@ namespace Reflectis.SDK.CreatorKit
     [CreateAssetMenu(menuName = "Reflectis/Scriptable Actions/TriggerEventOnVisualScripting", fileName = "TriggerEventOnVisualScripting")]
     public class EnableInteractionOnVisualScripting : AwaitableScriptableAction
     {
-        [SerializeField] string eventName;
+        [SerializeField] List<string> eventsName;
 
         public override Task Action(IInteractable interactable)
         {
@@ -17,7 +18,14 @@ namespace Reflectis.SDK.CreatorKit
                 return Task.CompletedTask;
             }
 
-            CustomEvent.Trigger(interactable.GameObjectRef, eventName, eventName);
+            if (eventsName.Count != 0 && eventsName != null)
+            {
+                foreach (var eventName in eventsName)
+                {
+                    if (!string.IsNullOrEmpty(eventName) && eventName != "")
+                        CustomEvent.Trigger(interactable.GameObjectRef, eventName, eventName);
+                }
+            }
 
             return Task.CompletedTask;
         }
