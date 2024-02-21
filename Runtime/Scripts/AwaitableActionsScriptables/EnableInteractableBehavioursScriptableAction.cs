0574595147c515e65846eb920aa606c5ec11982c
FrancescoWeik <fweikmann@gmail.com>
2024-02-21 12:45:48 +0100

    removed enable and disable of script

--- Runtime/Scripts/AwaitableActionsScriptables/EnableInteractableBehavioursScriptableAction.cs
+++ Runtime/Scripts/AwaitableActionsScriptables/EnableInteractableBehavioursScriptableAction.cs
@@ -22,7 +22,6 @@ namespace Reflectis.SDK.CreatorKit
                 {
                     if (beh is Manipulable manipulable && interactionsToEnable.HasFlag(EInteractableType.Manipulable))
                     {
-                        //TODO Refactor of CanInteract/Ownership/CanManipulate
                         if (!activate)
                         {
                             manipulable.CurrentBlockedState |= InteractableBehaviourBase.EBlockedState.BlockedBySelection;
@@ -30,10 +29,8 @@ namespace Reflectis.SDK.CreatorKit
                         else
                         {
                             manipulable.CurrentBlockedState = manipulable.CurrentBlockedState & ~InteractableBehaviourBase.EBlockedState.BlockedBySelection;
-                            //manipulable.CurrentBlockedState = InteractableBehaviourBase.EBlockedState.Idle;
-                            //remove blockedBySelection
                         }
-                        manipulable.enabled = activate;
+                        //manipulable.enabled = activate;
                     }
 
                     if (beh is GenericInteractable genericInteractable && interactionsToEnable.HasFlag(EInteractableType.GenericInteractable))
@@ -44,12 +41,9 @@ namespace Reflectis.SDK.CreatorKit
                         }
                         else
                         {
-                            //remove blockedBySelection
                             genericInteractable.CurrentBlockedState = genericInteractable.CurrentBlockedState &~InteractableBehaviourBase.EBlockedState.BlockedBySelection;
-                            //genericInteractable.CurrentBlockedState = InteractableBehaviourBase.EBlockedState.Idle;
                         }
-                        genericInteractable.enabled = activate;
-                        //genericInteractable.CanInteract = activate;
+                        //genericInteractable.enabled = activate;
                     }
 
                     if (beh is ContextualMenuManageable manageable && interactionsToEnable.HasFlag(EInteractableType.ContextualMenuInteractable))
@@ -60,11 +54,9 @@ namespace Reflectis.SDK.CreatorKit
                         }
                         else
                         {
-                            //remove blockedBySelection
                             manageable.CurrentBlockedState = manageable.CurrentBlockedState & ~InteractableBehaviourBase.EBlockedState.BlockedBySelection;
                         }
-                        manageable.enabled = activate;
-                        //manageable.CanInteract = activate;
+                        //manageable.enabled = activate;
                     }
                 };
             }
