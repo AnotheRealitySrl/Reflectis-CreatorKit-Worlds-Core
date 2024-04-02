8f4126b0e4ad3d6dc6dc40efa26256b4f68e4701
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2024-04-02 17:20:03 +0200

    Added reset custom ypes to Reflectis custom type setup and added Enable Character TagNode to mirror

--- Editor/Scripts/VisualScriptingCustomTypeCollector.cs
+++ Editor/Scripts/VisualScriptingCustomTypeCollector.cs
@@ -1,4 +1,8 @@
 #if UNITY_EDITOR
+using Reflectis.SDK.Utilities;
+using Reflectis.SDK.Utilities.Extensions;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Reflectis.SDK.CreatorKitEditor
@@ -7,9 +11,32 @@ namespace Reflectis.SDK.CreatorKitEditor
     public class VisualScriptingCustomTypeCollector : ScriptableObject
     {
         [SerializeField]
-        private TextAsset[] customTypeTexts;
+        private TextAsset[] customTypeTexts = new TextAsset[0];
+
+        [SerializeField]
+        private string[] customTypeNames = new string[0];
 
         public TextAsset[] CustomTypeTexts { get => customTypeTexts; set => customTypeTexts = value; }
+        public string[] CustomTypeNames { get => customTypeNames; set => customTypeNames = value; }
+
+        public List<Type> GetCustomTypes()
+        {
+            List<Type> types = new List<Type>();
+            foreach (TextAsset textAsset in customTypeTexts)
+            {
+                foreach (var typeName in textAsset.GetTextTypes())
+                {
+                    types.Add(TypeExtensions.GetTypeFromString(typeName));
+                }
+            }
+
+            foreach (string customTypeNames in customTypeNames)
+            {
+                types.Add(TypeExtensions.GetTypeFromString(customTypeNames));
+            }
+
+            return types;
+        }
     }
 }
 #endif
--- Editor/Scripts/VisualScriptingSetupEditor.cs
+++ Editor/Scripts/VisualScriptingSetupEditor.cs
@@ -1,7 +1,5 @@
 #if UNITY_EDITOR
 using Reflectis.SDK.CreatorKitEditor;
-using Reflectis.SDK.Utilities;
-using Reflectis.SDK.Utilities.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,72 +10,71 @@ using UnityEngine;
 public static class VisualScriptingSetupEditor
 {
     //[MenuItem("Reflectis/Reset Visual Scripting Nodes")]
-    //public static void ResetNodes()
-    //{
-    //    var _typeOptionsMetadata = BoltCore.Configuration.GetMetadata(nameof(BoltCore.Configuration.typeOptions));
-    //    _typeOptionsMetadata.Reset(true);
-    //}
+    public static void ResetNodes()
+    {
+        while (BoltCore.Configuration.typeOptions.Count > 0)
+        {
+            BoltCore.Configuration.typeOptions.RemoveAt(0);
+        }
+
+        var _typeOptionsMetadata = BoltCore.Configuration.GetMetadata(nameof(BoltCore.Configuration.typeOptions));
+        //_typeOptionsMetadata.Reset(true);
+        if (_typeOptionsMetadata.defaultValue is List<Type> defaultTypes)
+        {
+            foreach (Type type in defaultTypes)
+            {
+                BoltCore.Configuration.typeOptions.Add(type);
+            }
+        }
+    }
 
     [MenuItem("Reflectis/Setup Visual Scripting Nodes")]
     public static void Setup()
     {
-        var boltTypeOptionsChanged = false;
-        //var _typeOptionsMetadata = BoltCore.Configuration.GetMetadata(nameof(BoltCore.Configuration.typeOptions));
-        //_typeOptionsMetadata.Reset(true);
 
-        foreach (var typeTexts in GetCustomTypeTexts())
+        ResetNodes();
+
+        foreach (var type in GetCustomTypes())
         {
-            if (typeTexts == null)
+            if (type == null)
             {
-                Debug.Log("Found Null textAsset inside custom type list!");
+                Debug.Log("Found Null type!");
                 continue;
             }
-            foreach (var typeName in typeTexts.GetTextTypes())
+            if (type == null)
             {
-                Type type = TypeExtensions.GetTypeFromString(typeName);
-                if (type == null)
-                {
-                    continue;
-                }
-                if (!BoltCore.Configuration.typeOptions.Contains(type))
-                {
-                    if (type.Namespace != null && !BoltCore.Configuration.assemblyOptions.Exists(x => x.name == type.Namespace))
-                    {
-                        LooseAssemblyName looseAssemblyName = new LooseAssemblyName(type.Namespace);
-                        BoltCore.Configuration.assemblyOptions.Add(looseAssemblyName);
-                    }
-
-                    BoltCore.Configuration.typeOptions.Add(type);
-                    Debug.Log($"Add {type.FullName} to Visual Scripting type options.");
-                    boltTypeOptionsChanged = true;
-                }
+                continue;
             }
+            if (!BoltCore.Configuration.typeOptions.Contains(type))
+            {
+                if (type.Namespace != null && !BoltCore.Configuration.assemblyOptions.Exists(x => x.name == type.Namespace))
+                {
+                    LooseAssemblyName looseAssemblyName = new LooseAssemblyName(type.Namespace);
+                    BoltCore.Configuration.assemblyOptions.Add(looseAssemblyName);
+                }
+
+                BoltCore.Configuration.typeOptions.Add(type);
+                Debug.Log($"Add {type.FullName} to Visual Scripting type options.");
+            }
+
         }
 
-        if (boltTypeOptionsChanged)
-        {
-            var typeOptionsMetadata = BoltCore.Configuration.GetMetadata(nameof(BoltCore.Configuration.typeOptions));
-            typeOptionsMetadata.Save();
-            BoltCore.Configuration.Save();
-            Codebase.UpdateSettings();
-            UnitBase.Rebuild();
-        }
-        else
-        {
-            Debug.Log("Reflectis visual scripting nodes already up to date.");
-        }
-
+        var typeOptionsMetadata = BoltCore.Configuration.GetMetadata(nameof(BoltCore.Configuration.typeOptions));
+        typeOptionsMetadata.Save();
+        BoltCore.Configuration.Save();
+        Codebase.UpdateSettings();
+        UnitBase.Rebuild();
     }
 
-    private static IEnumerable<TextAsset> GetCustomTypeTexts()
+    private static IEnumerable<Type> GetCustomTypes()
     {
         var customTypeCollector = Resources.Load<VisualScriptingCustomTypeCollector>("VisualScriptingCustomTypeCollector");
         var reflectisCustomTypeCollector = Resources.Load<VisualScriptingCustomTypeCollector>("ReflectisVisualScriptingCustomTypeCollector");
         if (reflectisCustomTypeCollector != null)
         {
-            return customTypeCollector.CustomTypeTexts.Union(reflectisCustomTypeCollector.CustomTypeTexts);
+            return customTypeCollector.GetCustomTypes().Union(reflectisCustomTypeCollector.GetCustomTypes());
         }
-        return customTypeCollector.CustomTypeTexts;
+        return customTypeCollector.GetCustomTypes();
     }
 }
 #endif
