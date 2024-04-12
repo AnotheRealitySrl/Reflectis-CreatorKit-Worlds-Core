bb9932d8370b813951e6990f3b5c01a73d054a2b
FrancescoWeik <fweikmann@gmail.com>
2024-04-12 12:24:07 +0200

    Set the value on the graph flow in syncedInit

--- Runtime/VisualScripting/Scripts/Nodes/EventUnit/OnSyncedVariableInit.cs
+++ Runtime/VisualScripting/Scripts/Nodes/EventUnit/OnSyncedVariableInit.cs
@@ -115,6 +115,7 @@ namespace Reflectis.SDK.CreatorKit
             {
                 if (data.name == args.Item2)
                 {
+                    flow.SetValue(Value, data.Value);
                     if (args.Item1.variableSettings[i].hasChanged)
                     {
                         flow.SetValue(IsChanged, true);
