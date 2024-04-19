9905da01d70b0b56ec08dc2b630478ee843d141c
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2024-04-19 15:33:26 +0200

    Added comments on enable other characters and spawned objects nodes

---
+++ Editor/Scripts/VisualScripting/EnableOtherCharactersDescriptor.cs
@@ -0,0 +1,16 @@
+using Reflectis.SDK.CreatorKit;
+using Unity.VisualScripting;
+
+namespace Reflectis.SDK.CreatorKitEditor
+{
+    [Descriptor(typeof(EnableOtherCharactersNode))]
+    public class EnableOtherCharactersDescriptor : UnitDescriptor<EnableOtherCharactersNode>
+    {
+        public EnableOtherCharactersDescriptor(EnableOtherCharactersNode unit) : base(unit) { }
+
+        protected override string DefinedSummary()
+        {
+            return "NOT IMPLEMENTED YET!";
+        }
+    }
+}
