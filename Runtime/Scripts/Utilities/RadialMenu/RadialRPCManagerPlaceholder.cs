1d769c85eb4f6da46974ded5bc57abf1473226ef
FrancescoWeik <fweikmann@gmail.com>
2024-04-22 16:34:27 +0200

    update radialRPCManagerPlaceholder to handle items without references to them

--- Runtime/Scripts/Utilities/RadialMenu/RadialRPCManagerPlaceholder.cs
+++ Runtime/Scripts/Utilities/RadialMenu/RadialRPCManagerPlaceholder.cs
@@ -1,11 +1,10 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Reflectis.SDK.CreatorKit
 {
     public class RadialRPCManagerPlaceholder : SceneComponentPlaceholderNetwork
     {
-        [Tooltip("The models and only the models of the items that we want the other player to see spawned when we press on the icon if the item in the radial Menu. The list must have the items in the same order as the radial Menu")]
-        public List<GameObject> radialMenuItemModels;
+        [Tooltip("The radial rpc manager needs a reference to the radial menu in order to work correctly")]
+        public RadialMenuPlaceholder radialMenuPlaceholder; //reference to the radial menu
     }
 }
