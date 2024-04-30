7dbd486b80b8504ce3e422a3aea81e7b38046f4f
FrancescoWeik <fweikmann@gmail.com>
2024-04-30 10:09:22 +0200

    add radial spawner placeholder

---
+++ Runtime/Scripts/Utilities/RadialMenu/ReflectisRadialItemSpawnerPlaceholder.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Reflectis.SDK.CreatorKit
+{
+    public class ReflectisRadialItemSpawnerPlaceholder : SceneComponentPlaceholderNetwork
+    {
+        [Tooltip("The radial rpc manager needs a reference to the radial menu in order to work correctly")]
+        public RadialMenuPlaceholder radialMenuPlaceholder; //reference to the radial menu
+    }
+}
