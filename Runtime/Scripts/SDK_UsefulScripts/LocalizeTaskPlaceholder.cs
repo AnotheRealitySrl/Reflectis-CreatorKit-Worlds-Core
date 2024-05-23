3c826c60420f30c78201215c04b7b19cba928310
FrancescoWeik <fweikmann@gmail.com>
2024-05-23 17:55:26 +0200

    Commented image and videoKey in localizeTaskPlaceholder

--- Runtime/Scripts/SDK_UsefulScripts/LocalizeTaskPlaceholder.cs
+++ Runtime/Scripts/SDK_UsefulScripts/LocalizeTaskPlaceholder.cs
@@ -6,17 +6,17 @@ namespace Reflectis.SDK.CreatorKit
     {
         [SerializeField, Tooltip("Enable description localization")]
         public bool localizeDescription = false;
-        [SerializeField, Tooltip("Enable image localization")]
-        public bool localizeImage = false;
-        [SerializeField, Tooltip("Enable videoClip localization")]
-        public bool localizeVideo = false;
+        [Tooltip("Enable image localization")]
+        private bool localizeImage = false;
+        [Tooltip("Enable videoClip localization")]
+        private bool localizeVideo = false;
 
         [Space]
         [SerializeField, Tooltip("String key relative to the task's description")]
         public string descriptionKey;
-        [SerializeField, Tooltip("String key relative to the task's sprite")]
-        public string imageKey;
-        [SerializeField, Tooltip("String key relative to the task's video")]
-        public string videoKey;
+        [Tooltip("String key relative to the task's sprite")]
+        private string imageKey;
+        [Tooltip("String key relative to the task's video")]
+        private string videoKey;
     }
 }
