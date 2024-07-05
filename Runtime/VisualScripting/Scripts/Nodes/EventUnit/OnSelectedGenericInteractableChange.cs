1b66eb804f5530e1da49fa2819499ff762517ebb
Elia Russo <rockrono_x@yahoo.it>
2024-07-05 14:34:34 +0200

    New event node that triggers whenever the current generic interactable selection changes

---
+++ Runtime/VisualScripting/Scripts/Nodes/EventUnit/OnSelectedGenericInteractableChange.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using Reflectis.SDK.InteractionNew;
+using static Reflectis.SDK.InteractionNew.IInteractable;
+using Reflectis.SDK.Core;
+
+namespace Reflectis.SDK.CreatorKit
+{
+    [UnitTitle("Reflectis Generic Interactable: On Selected Change")]
+    [UnitSurtitle("GenericInteractable")]
+    [UnitShortTitle("On Selected Change")]
+    [UnitCategory("Events\\Reflectis")]
+    public class OnSelectedGenericInteractableChange : EventUnit<GenericInteractable>
+    {
+        [DoNotSerialize]
+        public ValueOutput GenericInteractable { get; private set; }
+        protected override bool register => true;
+
+        protected GraphReference graphReference;
+
+        protected GenericInteractable interactableReference;
+
+        protected override void Definition()
+        {
+            base.Definition();
+            // Setting the value on our port.
+            GenericInteractable = ValueOutput<GenericInteractable>(nameof(GenericInteractable));
+        }
+
+        protected override void AssignArguments(Flow flow, GenericInteractable data)
+        {
+            flow.SetValue(GenericInteractable, interactableReference);
+        }
+
+        public override EventHook GetHook(GraphReference reference)
+        {
+            graphReference = reference;
+
+            SM.GetSystem<IGenericInteractionSystem>().OnSelectedInteractableChange.AddListener(OnSelectedChange);
+
+            return new EventHook("GenericInteractable" + this.ToString().Split("EventUnit")[0]);
+        }
+
+        private void OnSelectedChange(GenericInteractable newSelection)
+        {
+            Trigger(graphReference, newSelection);
+        }
+    }
+}
