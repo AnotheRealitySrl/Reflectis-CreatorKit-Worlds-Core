f06af0884b3b21affca2d5ee0c4324017c9ff5b2
Verza-ROG <feelthecabbage@gmail.com>
2024-08-21 19:15:42 +0200

    Added nodes to expose quiz info.

---
+++ Runtime/VisualScripting/Scripts/Nodes/Unit/ExposeQuizAnswerUnit.cs
@@ -0,0 +1,82 @@
+﻿using Unity.VisualScripting;
+
+namespace Reflectis.SDK.CreatorKit
+{
+    [UnitTitle("Reflectis: Expose Quiz Answer")]
+    [UnitSurtitle("Expose")]
+    [UnitShortTitle("Quiz Answer")]
+    [UnitCategory("Reflectis\\Expose")]
+    public class ExposeQuizAnswerUnit : Unit
+    {
+        [NullMeansSelf]
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput Answer { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput TitleLabel { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput TitleValue { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput Image { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput CorrectAnswer { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput ScoreIfGood { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput ScoreIfBad { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput FeedbackLabel { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput FeedbackValue { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput IsSelected { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput IsCorrectSelection { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput Score { get; private set; }
+
+        protected override void Definition()
+        {
+            Answer = ValueInput<QuizAnswer>(nameof(Answer), null).NullMeansSelf();
+
+            // Anagraphics
+
+            TitleLabel = ValueOutput(nameof(TitleLabel), (flow) => flow.GetValue<QuizAnswer>(Answer).TitleLabel);
+
+            // ToDo: Fill with localized text
+            TitleValue = ValueOutput(nameof(TitleValue), (flow) => flow.GetValue<QuizAnswer>(Answer).TitleLabel);
+
+            Image = ValueOutput(nameof(Image), (flow) => flow.GetValue<QuizAnswer>(Answer).Image);
+
+            CorrectAnswer = ValueOutput(nameof(CorrectAnswer), (flow) => flow.GetValue<QuizAnswer>(Answer).CorrectAnswer);
+
+            ScoreIfGood = ValueOutput(nameof(ScoreIfGood), (flow) => flow.GetValue<QuizAnswer>(Answer).ScoreIfGood);
+
+            ScoreIfBad = ValueOutput(nameof(ScoreIfBad), (flow) => flow.GetValue<QuizAnswer>(Answer).ScoreIfBad);
+
+            FeedbackLabel = ValueOutput(nameof(FeedbackLabel), (flow) => flow.GetValue<QuizAnswer>(Answer).FeedbackLabel);
+
+            // ToDo: Fill with localized text
+            FeedbackValue = ValueOutput(nameof(FeedbackValue), (flow) => flow.GetValue<QuizAnswer>(Answer).FeedbackLabel);
+
+            // Instance-related info
+
+            IsSelected = ValueOutput(nameof(IsSelected), (flow) => flow.GetValue<QuizAnswer>(Answer).IsSelected);
+
+            IsCorrectSelection = ValueOutput(nameof(IsCorrectSelection), (flow) => flow.GetValue<QuizAnswer>(Answer).IsCorrectSelection);
+
+            Score = ValueOutput(nameof(Score), (flow) => flow.GetValue<QuizAnswer>(Answer).CurrentScore);
+        }
+    }
+}
---
+++ Runtime/VisualScripting/Scripts/Nodes/Unit/ExposeQuizInstanceUnit.cs
@@ -0,0 +1,83 @@
+﻿using Reflectis.SDK.InteractionNew;
+using Unity.VisualScripting;
+
+namespace Reflectis.SDK.CreatorKit
+{
+    [UnitTitle("Reflectis: Expose Quiz Instance")]
+    [UnitSurtitle("Expose")]
+    [UnitShortTitle("Quiz Instance")]
+    [UnitCategory("Reflectis\\Expose")]
+    public class ExposeQuizInstanceUnit : Unit
+    {
+        [NullMeansSelf]
+        [DoNotSerialize]
+        [PortLabelHidden]
+        public ValueInput Quiz { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput GameObjectReference { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput TitleLabel { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput TitleValue { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput DescriptionLabel { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput DescriptionValue { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput Answers { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput AnswersCount { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput ScoreIfAllGood { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput ScoreIfAllBad { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput CorrectAnswers { get; private set; }
+
+        [DoNotSerialize]
+        public ValueOutput Score { get; private set; }
+
+        protected override void Definition()
+        {
+            Quiz = ValueInput<QuizPlaceholder>(nameof(Quiz), null).NullMeansSelf();
+
+            GameObjectReference = ValueOutput(nameof(GameObjectReference), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).gameObject);
+
+            // Anagraphics
+
+            TitleLabel = ValueOutput(nameof(TitleLabel), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).TitleLabel);
+
+            // ToDo: Fill with localized text
+            TitleValue = ValueOutput(nameof(TitleValue), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).TitleLabel);
+
+            DescriptionLabel = ValueOutput(nameof(DescriptionLabel), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).DescriptionLabel);
+
+            // ToDo: Fill with localized text
+            DescriptionValue = ValueOutput(nameof(DescriptionValue), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).DescriptionLabel);
+
+            // Instance-related info
+
+            Answers = ValueOutput(nameof(Answers), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).QuizInstanceAnswers);
+
+            AnswersCount = ValueOutput(nameof(AnswersCount), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).QuizInstanceAnswersCount);
+
+            ScoreIfAllGood = ValueOutput(nameof(ScoreIfAllGood), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).QuizInstanceAllGoodScore);
+
+            ScoreIfAllBad = ValueOutput(nameof(ScoreIfAllBad), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).QuizInstanceAllBadScore);
+
+            CorrectAnswers = ValueOutput(nameof(CorrectAnswers), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).QuizInstanceCorrectAnswersCount);
+
+            Score = ValueOutput(nameof(Score), (flow) => flow.GetValue<QuizPlaceholder>(Quiz).QuizInstanceScore);
+        }
+    }
+}
