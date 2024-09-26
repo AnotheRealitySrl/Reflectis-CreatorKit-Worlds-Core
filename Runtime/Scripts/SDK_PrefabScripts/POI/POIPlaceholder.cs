f436ecf740ae3577149bf96eadd7e60e40436803
Emanuele Torelli <emanueletorelli@outlook.com>
2024-09-26 19:43:57 +0200

    POI initialization refinements

--- Runtime/Scripts/SDK_PrefabScripts/POI/POIPlaceholder.cs
+++ Runtime/Scripts/SDK_PrefabScripts/POI/POIPlaceholder.cs
@@ -8,11 +8,18 @@ namespace Reflectis.SDK.CreatorKit
 {
     public class POIPlaceholder : SceneComponentPlaceholderBase
     {
+        [Header("Read only references. Do no edit them, unless you need to do some heavy customizations.")]
+        [SerializeField] private RectTransform activator;
+        [SerializeField] private RectTransform panel;
+
         [HelpBox("To edit the content of each block of the POI, please double-click on the item in the list." +
             "You will be redirected to the placeholder where you can customize the content.", HelpBoxMessageType.Info)]
 
         [SerializeField] private List<POIBlockPlaceholder> poiElements;
 
         public List<POIBlockPlaceholder> POIElements => poiElements;
+
+        public Transform Activator => activator;
+        public Transform Panel => panel;
     }
 }
