32f62cb533ef1412592b19d18c1a274f8b4ecf84
Emanuele Torelli <emanueletorelli@outlook.com>
2024-11-04 17:01:12 +0100

    Add data to ChatBotPlaceholder

--- Runtime/Scripts/SDK_BehaviourScripts/ChatBotPlaceholder.cs
+++ Runtime/Scripts/SDK_BehaviourScripts/ChatBotPlaceholder.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Reflectis.SDK.CreatorKit
 {
     public class ChatBotPlaceholder : SceneComponentPlaceholderBase
     {
         [SerializeField] private string initialConversationSentence = "Hello!";
+        [SerializeField] private Transform panTarget;
+        [SerializeField] private RectTransform chatPanel;
 
         public string InitialConversationSentence => initialConversationSentence;
+        public Transform PanTarget => panTarget;
+        public RectTransform ChatPanel => chatPanel;
+
+        public UnityEvent OnSelectedEvent { get; } = new();
+        public UnityEvent OnDeselectedEvent { get; } = new();
     }
 }
