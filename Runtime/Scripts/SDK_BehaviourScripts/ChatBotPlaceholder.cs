3c02a1dea4294b3bccaf67b2178408fa55f1aff1
Emanuele Torelli <emanueletorelli@outlook.com>
2024-11-21 11:24:00 +0100

    Minor chatbot structure improvements

--- Runtime/Scripts/SDK_BehaviourScripts/ChatBotPlaceholder.cs
+++ Runtime/Scripts/SDK_BehaviourScripts/ChatBotPlaceholder.cs
@@ -23,6 +23,28 @@ namespace Reflectis.SDK.CreatorKit
 
     public class ChatBotPlaceholder : SceneComponentPlaceholderBase
     {
+        [HelpBox("How to configure a chatbot" +
+            "First of all, drag&drop one of the avatar templates inside the 'Avatar container' transform. " +
+            "Move ore resize it transform, if needed. " +
+            "IMPORTANT: to make the interaction work properly, " +
+            "Add a reference to the collider of the avatar to the 'InteractablePlaceholder''s collider list. " +
+            "Check that the avatar has a collider, needed for the interaction with the chatbot. " +
+            "If the avatar is a RPM avatar and you want to add eye-blink and lip-sync behaviors, " +
+            "add respectively the 'Eye Animation Handler' and 'Voice Handler' components to the avatar template root. " +
+            "Reference the audio source of the avatar inside the 'Voice Handler' component. " +
+            "If you want to move the audio source, where the audio of the avatar comes from, " +
+            "move it into a transform, keeping its reference to 'Voice Handler' component. " +
+            "Add an animator to the avatar, in which there can be multiple states, " +
+            "but there must be only one bool trigger named 'Speak'.")]
+
+        [Header("Chatbot structure. Do not change the references,\nunless you need to do heavy customizations.")]
+        [SerializeField] private Transform avatarContainer;
+        [SerializeField] private RectTransform chatPanel;
+
+        [Tooltip("Specify the animator in the case the avatar in use has multiple animators in the hierarchy.")]
+        [SerializeField] private Animator animator;
+
+
         [Header("Chatbot configuration")]
 
         [Header("This will be displayed in the UI panel.")]
@@ -44,22 +66,15 @@ namespace Reflectis.SDK.CreatorKit
 
         [SerializeField] private EChatBotVoice voice = EChatBotVoice.alloy;
 
-        [Header("Chatbot structure. Do not change the references, unless you need to do a custom avatar.")]
-        [SerializeField] private Transform panTarget;
-        [SerializeField] private RectTransform chatPanel;
-        [SerializeField] private AudioSource audioSource;
-        [SerializeField] private Animator avatarAnimator;
-
         public string ChatbotName => chatbotName;
         public bool StartTheConversation => startTheConversation;
         public string InitialConversationSentence => initialConversationSentence;
         public string Instructions => instructions;
         public EChatBotVoice Voice => voice;
 
-        public Transform PanTarget => panTarget;
+        public Transform AvatarContainer => avatarContainer;
         public RectTransform ChatPanel => chatPanel;
-        public AudioSource AudioSource => audioSource;
-        public Animator Animator => avatarAnimator;
+        public Animator Animator => animator;
 
         public UnityEvent OnChatBotSelect { get; } = new();
         public UnityEvent OnChatBotUnselected { get; } = new();
