a807eb1c1619b70390e21b25e3c546c9bf5172a1
Emanuele Torelli <emanueletorelli@outlook.com>
2025-02-04 15:25:37 +0100

    CK namespaces refactor

--- Runtime/ApplicationManagement/IReflectisApplicationManager.cs
+++ Runtime/ApplicationManagement/IReflectisApplicationManager.cs
@@ -1,6 +1,7 @@
 using Reflectis.CreatorKit.Worlds.Core.ClientModels;
 using Reflectis.SDK.Core.ApplicationManagement;
 
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 using UnityEngine;
@@ -10,9 +11,21 @@ namespace Reflectis.CreatorKit.Worlds.Core
     public interface IReflectisApplicationManager : IApplicationManager
     {
         static new IReflectisApplicationManager Instance { get; protected set; }
+
         EApplicationState State { get; }
+
         Task LoadDefaultEvent();
         Task InitializeObject(GameObject gameObject, bool initializeChildren = false);
         Task LoadEvent(CMEvent ev, CMShard shard = null, bool updateHistory = true, bool recoverFromDisconnection = false);
+
+        void EnableOtherAvatars(bool enable, List<GameObject> except = null);
+        void EnableSpawnedObjects(bool enable, List<GameObject> except = null);
+        void EnableSpawnedNetworkObjects(bool enable, List<GameObject> except = null);
+        void EnableSpawnedLocalObjects(bool enable, List<GameObject> except = null);
+        void EnableEnvironment(bool enable, List<GameObject> except = null);
+        void AddNetworkSpawnedObject(GameObject gameObject);
+        void AddAvatar(GameObject avatar);
+        void AddLocalSpawnedObject(GameObject localObject);
+        void AddEnvironmentObject(GameObject environmentObject);
     }
 }
