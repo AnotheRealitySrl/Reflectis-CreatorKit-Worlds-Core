1ef4311fd963fae4e7271ea33287cc03bba8d0aa
Francesco Weikmann <fweikmann@gmail.com>
2025-07-25 11:09:26 +0200

    add right hand reference, add positioning in spawnableObject

--- Runtime/Placeholders/SpawnableObject.cs
+++ Runtime/Placeholders/SpawnableObject.cs
@@ -12,14 +12,22 @@ namespace Reflectis.CreatorKit.Worlds.Core
     {
 
         private const string SpawnableObjectListDataPath = "Assets/SpawnableObject/SpawnableObjectList.asset";
-        private const string HandReferencePrefabPath = "Packages/com.anotherealitysrl.reflectis-creatorkit-worlds-core/Prefab/HandReference.prefab";
+        private const string LeftHandReferencePrefabPath = "Packages/com.anotherealitysrl.reflectis-creatorkit-worlds-core/Prefab/LeftHandReference.prefab";
+        private const string RightHandReferencePrefabPath = "Packages/com.anotherealitysrl.reflectis-creatorkit-worlds-core/Prefab/RightHandReference.prefab";
 
         private bool _isSceneObject;
         public bool IsSceneObject => _isSceneObject;
 
-        //in the future add spawnOnHand bool and if false spawnPoint transform.
+        private bool spawnInHand = true; //in the future add spawnOnHand bool and if false spawnPoint transform.
+        //------------ These 2 parameters will be hidden if spawnInHand is false.
+        public Vector3 leftSpawnPositionOffset = Vector3.zero; 
+        public Vector3 rightSpawnPositionOffset = Vector3.zero;
+        //------------------
 
-        private GameObject HandReference;
+        //If spawnInHand is false then show the parameter spawnPoint which is a Transform
+
+        /*[HideInInspector]*/ public GameObject LeftHandReference;
+        /*[HideInInspector]*/ public GameObject RightHandReference;
 
 
 #if UNITY_EDITOR
@@ -28,15 +36,6 @@ namespace Reflectis.CreatorKit.Worlds.Core
             if (Application.isPlaying || EditorApplication.isPlayingOrWillChangePlaymode)
             {
                 return;
-            }
-
-            //Add the hand reference
-            if(HandReference == null)
-            {
-                var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(HandReferencePrefabPath);
-                HandReference = (GameObject)PrefabUtility.InstantiatePrefab(prefab, this.transform);
-                HandReference.transform.localPosition = Vector3.zero;
-                EditorUtility.SetDirty(this);
             }
 
             //Check whether or not I am a prefab and I am in prefab mode 
@@ -70,6 +69,57 @@ namespace Reflectis.CreatorKit.Worlds.Core
             }
         }
 
+        private static SpawnableObjectListData LoadOrCreateData()
+        {
+            SpawnableObjectListData spawnableList = AssetDatabase.LoadAssetAtPath<SpawnableObjectListData>(SpawnableObjectListDataPath);
+            if (spawnableList == null)
+            {
+                // Ensure folder exists
+                string dir = Path.GetDirectoryName(SpawnableObjectListDataPath);
+                if (!Directory.Exists(dir))
+                    Directory.CreateDirectory(dir);
+
+                // Create instance and save
+                spawnableList = ScriptableObject.CreateInstance<SpawnableObjectListData>();
+                AssetDatabase.CreateAsset(spawnableList, SpawnableObjectListDataPath);
+            }
+
+            return spawnableList;
+        }
+
+        public void ShowHandReference(GameObject HandReference, int handType)
+        {
+            GameObject hand = null;
+            string path = "";
+            if (handType == 0){
+                path = LeftHandReferencePrefabPath;
+                hand = LeftHandReference;
+            }
+            else
+            {
+                path = RightHandReferencePrefabPath;
+                hand = RightHandReference;
+            }
+
+            //Add the hand reference
+            if (hand == null)
+            {
+                var prefab = AssetDatabase.LoadAssetAtPath<GameObject>(path);
+                hand = (GameObject)PrefabUtility.InstantiatePrefab(prefab, this.transform);
+                hand.transform.localPosition = Vector3.zero;
+                if (handType == 0)
+                    LeftHandReference = hand;
+                else
+                    RightHandReference = hand;
+                EditorUtility.SetDirty(this);
+            }
+            else
+            {
+                DestroyImmediate(hand.gameObject);
+                hand = null;
+            }
+        }
+
         /*private void OnDestroy()
         {
             // Only handle editor-time logic (not during play mode)
@@ -89,26 +139,41 @@ namespace Reflectis.CreatorKit.Worlds.Core
                 Debug.Log($"Removed {name} from spawnable list (component removed or destroyed)");
             }
         }*/
+#endif
+    }
 
-        private static SpawnableObjectListData LoadOrCreateData()
+#if UNITY_EDITOR
+    [CustomEditor(typeof(SpawnableObject))]
+    public class SpawnableObjectEditor : Editor
+    {
+        public override void OnInspectorGUI()
         {
-            SpawnableObjectListData spawnableList = AssetDatabase.LoadAssetAtPath<SpawnableObjectListData>(SpawnableObjectListDataPath);
-            if (spawnableList == null)
-            {
-                // Ensure folder exists
-                string dir = Path.GetDirectoryName(SpawnableObjectListDataPath);
-                if (!Directory.Exists(dir))
-                    Directory.CreateDirectory(dir);
+            DrawDefaultInspector();
 
-                // Create instance and save
-                spawnableList = ScriptableObject.CreateInstance<SpawnableObjectListData>();
-                AssetDatabase.CreateAsset(spawnableList, SpawnableObjectListDataPath);
+            SpawnableObject myScript = (SpawnableObject)target;
+            string leftButtonLabel = myScript.LeftHandReference == null ? "Show Left Reference" : "Hide Left Reference";
+            string rigthButtonLabel = myScript.RightHandReference == null ? "Show Right Reference" : "Hide Right Reference";
+
+            if (myScript.LeftHandReference != null)
+            {
+                //place the hand based on the position set by the user
+                myScript.LeftHandReference.transform.position = myScript.leftSpawnPositionOffset;
+            }
+            if (myScript.RightHandReference != null)
+            {
+                //place the hand based on the position set by the user
+                myScript.RightHandReference.transform.position = myScript.rightSpawnPositionOffset;
             }
 
-            return spawnableList;
+            if (GUILayout.Button(leftButtonLabel))
+            {
+                myScript.ShowHandReference(myScript.LeftHandReference, 0);
+            }
+            if (GUILayout.Button(rigthButtonLabel))
+            {
+                myScript.ShowHandReference(myScript.RightHandReference, 1);
+            }
         }
-#endif
-
-
     }
+#endif
 }
