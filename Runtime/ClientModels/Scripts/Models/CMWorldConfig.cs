4caaccfb3e204d526412afcdc6fd5803467b2f33
Raffaele Tolusso <raffaele.tolusso@gmail.com>
2025-07-29 18:09:24 +0200

    Fix on new world facets

--- Runtime/ClientModels/Scripts/Models/CMWorldConfig.cs
+++ Runtime/ClientModels/Scripts/Models/CMWorldConfig.cs
@@ -13,12 +13,16 @@ namespace Reflectis.CreatorKit.Worlds.Core.ClientModels
         [SerializeField] private bool allowDoubleUserPresence;
         [SerializeField] private bool showFullNickname;
         [SerializeField] private int maxShardCapacity = 20;
+        [SerializeField] private EWorldMode worldMode = EWorldMode.Catalogo;
 
         public string VideoChatAppId { get => videoChatAppId; set => videoChatAppId = value; }
         public string GptAppId { get => gptAppId; set => gptAppId = value; }
-        public bool AllowDoubleUserPresence { get => allowDoubleUserPresence; set => allowDoubleUserPresence = value; }
-        public bool ShowFullNickname { get => showFullNickname; set => showFullNickname = value; }
         public int MaxShardCapacity { get => maxShardCapacity; set => maxShardCapacity = value; }
 
+        public enum EWorldMode
+        {
+            Catalogo,
+            Play
+        }
     }
 }
