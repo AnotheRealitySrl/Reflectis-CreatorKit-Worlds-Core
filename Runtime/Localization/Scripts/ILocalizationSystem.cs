0dfb3c354c41fd432a8f8aeeaa03449552716a80
Francesco Weikmann <fweikmann@gmail.com>
2025-07-29 16:10:33 +0200

    add function for localizationsystem

--- Runtime/Localization/Scripts/ILocalizationSystem.cs
+++ Runtime/Localization/Scripts/ILocalizationSystem.cs
@@ -1,12 +1,18 @@
 using Reflectis.SDK.Core.SystemFramework;
 using System.Collections.Generic;
+using UnityEngine.Events;
 
 namespace Reflectis.CreatorKit.Worlds.Core.Localization
 {
     public interface ILocalizationSystem : ISystem
     {
+        public UnityEvent<string> OnLanguageChanged { get; set; }
         public string GetCurrentLocalization();
         public string GetCurrentLanguageCode();
+        public string GetPreviousLanguage();
+        public string GetPreviousLanguageCode();
         public List<string> GetLanguagesList();
+        public void LanguageChangedEvent();
+        public void SetPreviousLanguage();
     }
 }
