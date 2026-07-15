a3d393a128d04fa2fe7304c51bfd3704f3aa209b
Francesco Weikmann <fweikmann@gmail.com>
2026-07-15 15:30:41 +0200

    add quiz to objectSpawnerSystem

--- Runtime/ObjectSpawner/Scripts/IObjectSpawnerSystem.cs
+++ Runtime/ObjectSpawner/Scripts/IObjectSpawnerSystem.cs
@@ -21,7 +21,8 @@ namespace Reflectis.CreatorKit.Worlds.Core.ObjectSpawner
             TextBox,
             GeneralContainer,
             AvatarStandard,
-            AvatarXR
+            AvatarXR,
+            Quiz
         }
 
         GameObject InstantiateLocalObject(GameObject gameObject, object[] data = null, SpawnPosition spawnableData = null);
